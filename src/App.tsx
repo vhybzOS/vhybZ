2ebb88304002d9c8a6af8723e7ff2071ffdd724b
Ramin Barati <rekino@gmail.com>
2025-06-02 14:45:22 +0200

    init vite, react and ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>VhybZ + React</h1>
+      <p>
+        Initialization is done!
+      </p>
+    </>
+  )
+}
+
+export default App
