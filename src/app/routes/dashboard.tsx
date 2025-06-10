1dc1da65b00b4a7bab92a5f431a725b1f5660003
Keyvan M. Sadeghi <keyvan.m.sadeghi@gmail.com>
2025-06-10 17:27:19 -0400

    Bring in assistant-ui

--- src/app/routes/dashboard.tsx
+++ src/app/routes/dashboard.tsx
@@ -1,33 +1,39 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { SiteHeader } from "@/components/site-header"
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { Thread } from "@/components/thread";
+import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import {
   SidebarInset,
   SidebarProvider,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 
 export const iframeHeight = "800px"
 
 export const description = "A sidebar with a header and a search form."
 
 export default function Page() {
+  const runtime = useChatRuntime({
+    api: "/api/chat",
+  });
+
   return (
-    <div className="[--header-height:calc(--spacing(14))]">
+    <div className="h-dvh flex flex-col [--header-height:calc(--spacing(14))]">
       <SidebarProvider className="flex flex-col">
         <SiteHeader />
-        <div className="flex flex-1">
+        <div className="flex flex-1 overflow-hidden">
           <AppSidebar />
           <SidebarInset>
-            <div className="flex flex-1 flex-col gap-4 p-4">
-              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="flex h-full flex-col gap-4 p-4">
+              <div className="bg-muted/50 flex h-full flex-col rounded-xl">
+                <AssistantRuntimeProvider runtime={runtime}>
+                  <Thread />
+                </AssistantRuntimeProvider>
               </div>
-              <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
             </div>
           </SidebarInset>
         </div>
       </SidebarProvider>
     </div>
-  )
+  );
 }
