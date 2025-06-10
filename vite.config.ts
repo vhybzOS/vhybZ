ffb546f722da1b9c49397c3056cd4a2e71c256d0
Keyvan M. Sadeghi <keyvan.m.sadeghi@gmail.com>
2025-06-10 15:21:09 -0400

    Fix shadcn blocks

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
+import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    reactRouter(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
