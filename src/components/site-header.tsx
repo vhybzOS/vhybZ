44fcdf2a806b5ca24001340f16a8aab83b18427a
Keyvan M. Sadeghi <keyvan.m.sadeghi@gmail.com>
2025-06-10 21:38:29 -0400

    Add dark mode toggle, remove breadcrumb

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -12,6 +12,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { useSidebar } from "@/components/ui/sidebar"
+import { ModeToggle } from "@/components/ui/mode-toggle"
 
 export function SiteHeader() {
   const { toggleSidebar } = useSidebar()
@@ -28,20 +29,8 @@ export function SiteHeader() {
           <SidebarIcon />
         </Button>
         <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb className="hidden sm:block">
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
         <SearchForm className="w-full sm:ml-auto sm:w-auto" />
+        <ModeToggle />
       </div>
     </header>
   )
