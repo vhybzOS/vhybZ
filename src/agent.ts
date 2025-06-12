752cbd742a94eee9911d0a1d1f8dce9501d8f5aa
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2025-06-12 12:34:11 +0330

    remake what we had in fmg react native

--- src/agent.ts
+++ src/agent.ts
@@ -1,12 +1,22 @@
 import { StateGraph, START, MemorySaver, END } from "@langchain/langgraph"
-import { NamedMessages, Jodi, Human } from "nodes"
+import { NamedMessages, Jodi, Human, Davici, GraphState } from "nodes"
 
 
 const builder = new StateGraph(NamedMessages)
   .addNode("jodiN", Jodi)
-  .addNode("input", Human, { ends: ["jodiN", END] })
-  .addEdge(START, "input")
-  .addEdge("jodiN", "input")
+  .addNode("input", Human, { ends: ["jodiN", "daviciN", END] })
+  .addEdge(START, "jodiN")
+  .addConditionalEdges("jodiN", (state: GraphState) => {
+    if (state.jodi.length > 1 && state.jodi.at(-1)) {
+      const msg = state.jodi.at(-1)
+      if (msg?.text.includes("LETS BUILD THIS")) {
+        return "daviciN"
+      }
+    }
+    return "input"
+  })
+  .addNode("daviciN", Davici)
+  .addEdge("daviciN", "input")
 
 const checkpointer = new MemorySaver()
 
--- src/nodes.ts
+++ src/nodes.ts
@@ -1,40 +1,82 @@
-import { AIMessage, BaseMessage } from "@langchain/core/messages";
+import { AIMessage, BaseMessage, HumanMessage } from "@langchain/core/messages";
 import { Annotation, Command, interrupt, messagesStateReducer } from "@langchain/langgraph";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { RunnableConfig } from "@langchain/core/runnables";
+import { Runnable, RunnableConfig } from "@langchain/core/runnables";
 import { renderTemplate } from "./prompt";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { tools } from "tools";
 
 export const NamedMessages = Annotation.Root({
   jodi: Annotation<BaseMessage[]>({
     reducer: messagesStateReducer,
     default: () => []
   }),
+  davici: Annotation<BaseMessage[]>({
+    reducer: messagesStateReducer,
+    default: () => []
+  }),
   lastAgent: Annotation<string>({
     reducer: (_, y) => y,
-  })
+  }),
+  html: Annotation<string>
 })
 
 export type GraphState = typeof NamedMessages.State
 
+const toolNode = new ToolNode(tools)
 
 const llm = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-preview-05-20",
   maxRetries: 3,
+  temperature: 0.7
 });
 
+async function callLLM(model: Runnable, msgs: BaseMessage[], config?: RunnableConfig): Promise<BaseMessage[]> {
+  const resp = await model.invoke(msgs, config)
+  console.log("failed here")
+  const newMsgs = msgs.slice()
+  newMsgs.push(resp)
+  if (resp.tool_calls && resp.tool_calls.length > 0) {
+    const tresp = await toolNode.invoke({ messages: [resp] })
+    console.log("tool respoonse", tresp)
+    newMsgs.push(...tresp.messages)
+    console.log(newMsgs)
+    return await callLLM(model, newMsgs)
+  }
+  return newMsgs
+}
 
 export async function Jodi(state: GraphState, config?: RunnableConfig) {
-  const name = "jodi"
-  const prompt = await renderTemplate(name, {})
+  const name = "jodiN"
+  const prompt = await renderTemplate("jodi", {})
   const msgs: BaseMessage[] = []
-  if (state.jodi.length < 2 && prompt) {
+  if (state.jodi.length < 1 && prompt) {
     msgs.push(new AIMessage({ content: prompt }))
+    return { jodi: msgs, lastAgent: name }
   }
   msgs.push(...state.jodi)
-  const resp = await llm.invoke(msgs, config)
+  console.log("msgs length before", msgs.length)
+  const llmResp = await callLLM(llm, msgs)
+  console.log("msgs length after", msgs.length)
+  console.log(llmResp)
+  return { jodi: llmResp, lastAgent: name }
+}
 
-  msgs.push(resp)
-  return { jodi: msgs, lastAgent: name }
+export async function Davici(state: GraphState, config?: RunnableConfig) {
+  const name = "daviciN"
+  const prompt = await renderTemplate("davici", {})
+
+  const msgs: BaseMessage[] = []
+  if (state.davici.length < 1 && prompt) {
+    msgs.push(new AIMessage({ content: prompt }))
+    const { content } = state.jodi.at(-1)!
+    msgs.push(new HumanMessage({ content }))
+  }
+  msgs.push(...state.davici)
+  console.log(msgs)
+  const llmWithTools = llm.bindTools(tools)
+  return { davici: await callLLM(llmWithTools, msgs), lastAgent: name }
+
 }
 
 
@@ -45,7 +87,7 @@ export function Human(state: GraphState): Command {
     // throw new Error("Could not determine the active agent.")
     agent = state.lastAgent
   }
-
+  console.log(agent)
   const command = new Command({
     goto: agent,
     update: {
